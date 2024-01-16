d0ced53564dcf0eac21b890d6a0d16071f59413a
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-01-16 19:41:41 +0545

    bg image

--- src/Components/pages/Education.js
+++ src/Components/pages/Education.js
@@ -5,18 +5,21 @@ export default function Education() {
   return (
     <div className="eduContainer">
       <div className="leftDiv">
-        <div className="topContent">
+        <div className="topContent" id="topContent"></div>
+        <div className="schoolName">
           <h1>Siddhasthali English Boarding H.S.School</h1>
           <p>Sindhuli Madhi, 2</p>
         </div>
-        <div className="bottomContent">
+        <div className="bottomContent"></div>
+        <div className="hsebName">
           <h1>Milestone International College</h1>
           <p>Balkumari, Lalitpur</p>
         </div>
       </div>
       <div className="vl"></div>
       <div className="rightDiv">
-        <div className="soloContent">
+        <div className="soloContent"></div>
+        <div className="collegeName">
           <h1>Nepal College of Information Technology</h1>
           <p>Balkumari, Lalitpur</p>
         </div>
