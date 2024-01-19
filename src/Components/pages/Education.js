f57b107d92dd86b3a06a079c8510508d9b4a5b4a
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-01-19 19:55:58 +0545

    changed educations to card only

--- src/Components/pages/Education.js
+++ src/Components/pages/Education.js
@@ -5,23 +5,33 @@ export default function Education() {
   return (
     <div className="eduContainer">
       <div className="leftDiv">
-        <div className="topContent" id="topContent"></div>
-        <div className="schoolName">
+        <div className="building">
+          <h2>SLC</h2>
           <h1>Siddhasthali English Boarding H.S.School</h1>
-          <p>Sindhuli Madhi, 2</p>
+
+          <p>
+            Sindhuli Madhi, 2 <span className="completedYear">2072 B.S.</span>
+          </p>
         </div>
-        <div className="bottomContent"></div>
-        <div className="hsebName">
+
+        <div className="building">
+          <h2>HSEB</h2>
           <h1>Milestone International College</h1>
-          <p>Balkumari, Lalitpur</p>
+          <p>
+            Balkumari, Lalitpur <span className="completedYear">2075 B.S.</span>
+          </p>
         </div>
       </div>
+
       <div className="vl"></div>
+
       <div className="rightDiv">
-        <div className="soloContent"></div>
-        <div className="collegeName">
+        <div className="building">
+          <h2>Bachelors</h2>
           <h1>Nepal College of Information Technology</h1>
-          <p>Balkumari, Lalitpur</p>
+          <p>
+            Balkumari, Lalitpur <span className="completedYear">2080 B.S.</span>
+          </p>
         </div>
       </div>
     </div>
